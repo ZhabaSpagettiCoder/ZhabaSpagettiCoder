6fd1375db404ead2723ae85d20f1ee8766b2bf23
ZhabaSpagettiCoder <grigoryzhaba@gmail.com>
2024-10-16 13:46:08 +0300

    Err

--- .venv/Scripts/EduPractice/1.sql
+++ .venv/Scripts/EduPractice/1.sql
@@ -1,2 +1,2 @@
 
-inserting into InputDataRequests VALUES(1,01-01-2024,"This computer starting with peek sound and suddenly turn off","In progress", 0 ,05-01-2024,"CoolMaster","AAB","AAA")
+insert into InputDataRequests VALUES(1,01-01-2024,"This computer starting with peek sound and suddenly turn off","In progress", 0 ,05-01-2024,"CoolMaster","AAB","AAA")
